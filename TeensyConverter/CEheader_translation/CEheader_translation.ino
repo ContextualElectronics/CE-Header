00c2363ca9bd4ecf612b3fce3cd8337f79a76a5e
Chris Gammell <chris@contextualelectronics.com>
2017-10-25 00:25:56 -0500

    Created translation layer for CE header in Arduino

---
+++ TeensyConverter/CEheader_translation/CEheader_translation.ino
@@ -0,0 +1,22 @@
+#define CE_spi_cs_gpio3 10
+#define CE_spi_mosi_gpio0 11
+#define CE_spi_miso_gpio1 12
+#define CE_spi_clk_gpio2 12
+#define CE_usart_rx_gpio4 1
+#define CE_usart_tx_gpio5 0
+#define CE_i2c_sclk_gpio6 19
+#define CE_i2c_sdat_gpio7 18
+#define CE_aio0_gpio8 14
+#define CE_aio1_gpio9 15
+#define CE_aio2_gpio10 16
+#define CE_aio3_gpio11 17
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
